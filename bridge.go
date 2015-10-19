5cedb8d27ee56ff5d474d97ce1cc40b38b9f27d7
Garret Buell <gmbuell@gmail.com>
2015-10-19 13:33:32 -0700

    Add more methods to the lights api.

--- bridge.go
+++ bridge.go
@@ -1,7 +1,9 @@
 package hue
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/url"
 	"path"
@@ -18,7 +20,7 @@ func NewBridge(host string, user string) *Bridge {
 	return &Bridge{host: host, user: user, baseUrl: url.URL{Scheme: "http", Host: host, Path: path.Join("api", user)}}
 }
 
-func (bridge *Bridge) GetLights() (Lights, error) {
+func (bridge *Bridge) GetAllLights() (Lights, error) {
 	lightsUrl := bridge.baseUrl
 	lightsUrl.Path = path.Join(lightsUrl.Path, "lights")
 	resp, err := bridge.client.Get(lightsUrl.String())
@@ -34,4 +36,60 @@ func (bridge *Bridge) GetLights() (Lights, error) {
 		return nil, err
 	}
 	return lights, nil
+}
+
+func (bridge *Bridge) GetLight(id string) (*Light, error) {
+	lightsUrl := bridge.baseUrl
+	lightsUrl.Path = path.Join(lightsUrl.Path, "lights", id)
+	resp, err := bridge.client.Get(lightsUrl.String())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Unmarshal
+	light := Light{bridge: bridge, index: id}
+	err = json.NewDecoder(resp.Body).Decode(&light)
+	if err != nil {
+		return nil, err
+	}
+	return &light, nil
+}
+
+func (bridge *Bridge) SetLightName(id string, name string) error {
+	lightsUrl := bridge.baseUrl
+	lightsUrl.Path = path.Join(lightsUrl.Path, "lights", id)
+	var putBody bytes.Buffer
+	err := json.NewEncoder(&putBody).Encode(map[string]string{"name": name})
+	if err != nil {
+		return err
+	}
+
+	setRequest, err := http.NewRequest("PUT", lightsUrl.String(), &putBody)
+	if err != nil {
+		return err
+	}
+
+	resp, err := bridge.client.Do(setRequest)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Unmarshal
+	var response []map[string]map[string]string
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return err
+	}
+
+	if len(response) != 1 {
+		return errors.New("Expected one result in SetLightName response.")
+	}
+
+	if _, ok := response[0]["success"]; !ok {
+		return errors.New("API did not return success")
+	}
+
+	return nil
 }
--- lights.go
+++ lights.go
@@ -44,9 +44,29 @@ type Light struct {
 		Saturation       uint8     `json:"sat"`
 		XY               []float64 `json:"xy"`
 	} `json:"state"`
-	SoftwareVersion   string `json:"swversion"`
-	Type              string `json:"type"`
-	UniqueId          string `json:"uniqueid"`
-	ManufacturerName  string `json:"manufacturername"`
-	LuminaireUniqueId string `json:"luminaireuniqueid"`
+	SoftwareVersion   string  `json:"swversion"`
+	Type              string  `json:"type"`
+	UniqueId          string  `json:"uniqueid"`
+	ManufacturerName  string  `json:"manufacturername"`
+	LuminaireUniqueId string  `json:"luminaireuniqueid"`
+	bridge            *Bridge `json:"-"`
+}
+
+type SetState struct {
+	Alert                 string    `json:"alert,omitempty"`
+	Brightness            uint8     `json:"bri,omitempty"`
+	ColorMode             string    `json:"colormode,omitempty"`
+	ColorTemperature      uint16    `json:"ct,omitempty"`
+	Effect                string    `json:"effect,omitempty"`
+	Hue                   uint16    `json:"hue,omitempty"`
+	On                    bool      `json:"on,omitempty"`
+	Reachable             bool      `json:"reachable,omitempty"`
+	Saturation            uint8     `json:"sat,omitempty"`
+	XY                    []float64 `json:"xy,omitempty"`
+	TransitionTime        uint16    `json:"transitiontime,omitempty"`
+	BrightnessDelta       int16     `json:"bri_inc,omitempty"`
+	SaturationDelta       int16     `json:"sat_inc,omitempty"`
+	HueDelta              int16     `json:"hue_inc,omitempty"`
+	ColorTemperatureDelta int16     `json:"ct_inc,omitempty"`
+	XYDelta               []float64 `json:"xy_inc,omitempty"`
 }
